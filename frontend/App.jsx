f1f263ada3750e5ceacdde9f7dd5a2e2edd707ca
Manul Thisuraka <88134073+ManulThisuraka@users.noreply.github.com>
2022-06-01 23:39:31 +0530

    frontend render issue fixed

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import {BrowserRouter, Routes, Route, Router} from 'react-router-dom';
+import {BrowserRouter as Router , Routes, Route } from 'react-router-dom';
 import UserContext from "./src/context/UserContext";
 
 import LoginPage from "./src/pages/LoginPage";
@@ -8,45 +8,15 @@ import { render } from "react-dom";
 
 export default class App extends React.Component {
 
+
+
   render() {
     return (
-      <BrowserRouter>
-        <Router>
-          <Route path="/user/login" exact component={LoginPage} />
-        </Router>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/user/login" element={<LoginPage/>}/>
+        </Routes>
+      </Router>
     );
   }
 }
-
-
-  // const [userData, setUserData] = useState({
-  //   token: undefined,
-  //   user: undefined,
-  // });
-
-  // useEffect(() => {
-  //   const checkLoggedIn = async () => {
-  //     let token = localStorage.getItem("auth-token");
-  //     if (token === null) {
-  //       localStorage.setItem("auth-token", "");
-  //       token = "";
-  //     }
-  //     const tokenResponse = await axios.post(
-  //       "http://localhost:5000/users/tokenIsValid",
-  //       null,
-  //       { headers: { "x-auth-token": token } }
-  //     );
-  //     if (tokenResponse.data) {
-  //       const userRes = await axios.get("http://localhost:5000/users/", {
-  //         headers: { "x-auth-token": token },
-  //       });
-  //       setUserData({
-  //         token,
-  //         user: userRes.data,
-  //       });
-  //     }
-  //   };
-
-  //   checkLoggedIn();
- // // }, []);

e77ac5b4d2014b356d1a87b8c11b7e7e9ead82da
Manul Thisuraka <88134073+ManulThisuraka@users.noreply.github.com>
2022-06-01 22:57:33 +0530

    parcel cache removed

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
-export default class App extends React.Component{
-    render() {
-        return ( 
-            <div>
-                <h1>Hello. This is parcel react app</h1>
-            </div>
-        )
-    }
+import React from "react";
+import axios from "axios";
+import {BrowserRouter, Routes, Route, Router} from 'react-router-dom';
+import UserContext from "./src/context/UserContext";
+
+import LoginPage from "./src/pages/LoginPage";
+import { render } from "react-dom";
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Router>
+          <Route path="/user/login" exact component={LoginPage} />
+        </Router>
+      </BrowserRouter>
+    );
+  }
 }
+
+
+  // const [userData, setUserData] = useState({
+  //   token: undefined,
+  //   user: undefined,
+  // });
+
+  // useEffect(() => {
+  //   const checkLoggedIn = async () => {
+  //     let token = localStorage.getItem("auth-token");
+  //     if (token === null) {
+  //       localStorage.setItem("auth-token", "");
+  //       token = "";
+  //     }
+  //     const tokenResponse = await axios.post(
+  //       "http://localhost:5000/users/tokenIsValid",
+  //       null,
+  //       { headers: { "x-auth-token": token } }
+  //     );
+  //     if (tokenResponse.data) {
+  //       const userRes = await axios.get("http://localhost:5000/users/", {
+  //         headers: { "x-auth-token": token },
+  //       });
+  //       setUserData({
+  //         token,
+  //         user: userRes.data,
+  //       });
+  //     }
+  //   };
+
+  //   checkLoggedIn();
+ // // }, []);
